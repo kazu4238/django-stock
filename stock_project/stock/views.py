a34ea884c32be1e5d200033defe0d5cb12127f4c
kazu4238 <>
2020-11-15 23:42:29 +0900

    stock project作成

--- stock_project/stock/views.py
+++ stock_project/stock/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# home関数の追加
+# render関数は、第二引数に指定したテンプレートを読み込み、テンプレートに記述されている
+# データを表示できるように変換する処理を行う。
+# 第三引数に、辞書型を指定することで、テンプレートで{{}}という形で
+# 値を埋め込めるようにできます。(今回は空)
+def home(request):
+    return render(request,'hoge.html',{})
