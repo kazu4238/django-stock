33683bcf2c1c242f89be8e3a6839bddcabbd2ae5
kazu4238 <>
2020-11-16 11:57:52 +0900

    銘柄コード入力後に銘柄情報を表示させる

--- stock_project/stock/views.py
+++ stock_project/stock/views.py
@@ -10,12 +10,19 @@ Mytoken = "pk_be483a022b2f4e7699de538fc0131481"
 def home(request):
     import requests
     import json
-    # request.get(APIアドレス)でAPIデータをResponseオブジェクトとして取得している
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=" + Mytoken)
-    try:
-        # 取得したAPIデータをjSON形式に変換して読み込み、変数apiに格納する
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "エラー"
 
-    return render(request,'home.html',{'api':api})
+    if request.method == 'POST':
+        # target_symbolに選択した銘柄記号が入ることでその銘柄記号のAPIが取得できるようにする
+        target_symbol = request.POST['target_stock']
+        # request.get(APIアドレス)でAPIデータをResponseオブジェクトとして取得している
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + target_symbol + "/quote?token=" + Mytoken)
+        try:
+            # 取得したAPIデータをjSON形式に変換して読み込み、変数apiに格納する
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "エラー"
+
+        return render(request,'home.html',{'api':api})
+    else:
+        return render(request,'home.html')
